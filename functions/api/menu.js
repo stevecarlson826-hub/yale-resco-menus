604283f1d87c15db7bfc0602d440275597cceaec
stevecarlson826-hub <stevecarlson826@gmail.com>
2026-09-25 18:43:15 -0500

    Update menu.js

--- functions/api/menu.js
+++ functions/api/menu.js
@@ -23,7 +23,13 @@ export async function onRequestGet({ request }) {
 
   if (
     !ALLOWED.has(slug) ||
-    !["lunch", "dinner"].includes(meal) ||
+    ![
+      "breakfast",
+      "lunch",
+      "dinner",
+      "salad-bar",
+      "desserts-daily-offerings"
+     ].includes(meal) ||
     !/^\d{4}-\d{2}-\d{2}$/.test(date || "")
   ) {
     return new Response("Bad request", { status: 400 });
